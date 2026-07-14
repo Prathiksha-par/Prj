5412a5fb0135c30bb9d6e4f102de1eba2747ec81
Prathiksha-par <bharanics@srcw.ac.in>
2026-07-14 13:54:20 +0530

    Added calculator demo

--- cal.py
+++ cal.py
@@ -6,3 +6,17 @@ b = 20
 print("Addition =", a + b)
 
 print("CI Pipeline Executed Successfully")
+
+print("************************")
+print("WELCOME TO DEVOPS DEMO")
+print("************************")
+
+name = "Bharani"
+
+print("Developer :", name)
+
+marks = [80, 90, 95]
+
+print("Average =", sum(marks)/len(marks))
+
+print("Build Completed Successfully")
